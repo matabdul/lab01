96fd837a222227e1cde0d2ad47b2874f5b68fd64
matabdul <muftau.abdurrahmon@outlook.com>
2025-06-08 15:54:46 +0300

    Updated function to use tag

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let crypto = require("node:crypto");
 function releaseName(name, tag=10){
-    return name + ":" + crypto.randomBytes(16).toString('hex')
+    return name + ":" + crypto.randomBytes(tag).toString('hex')
 }
 console.log("Cloud Native application deployment!")
 console.log("Updated content")
