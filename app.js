1027ec912bd65e3297683a901386210bc44ac5ce
matabdul <muftau.abdurrahmon@outlook.com>
2025-06-08 15:52:48 +0300

    Added release note

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
+let crypto = require("node:crypto");
+function releaseName(name, tag=10){
+    return name + ":" + crypto.randomBytes(16).toString('hex')
+}
 console.log("Cloud Native application deployment!")
 console.log("Updated content")
+console.log("Release note: " + releaseName("Sample-release"))
